64ac32a2db93f0465670181b79f6110ef8a2e112
frosDfurret <64030476+frosDfurret@users.noreply.github.com>
2021-05-31 20:48:29 -0700

    Update app.js

--- bin/app.js
+++ bin/app.js
@@ -8,6 +8,8 @@ const Table = require('cli-table');
 const guerrillamail_url = 'https://www.guerrillamail.com/en';
 const proton_url = 'https://account.protonvpn.com/signup';
 
+
+
 let options = {
   confirmation: {
     type: "confirm",
@@ -16,9 +18,10 @@ let options = {
   }
 }
 
-const { version } = require('./../package.json');
-console.log(boxen('ProtonVPN Generator ' + version, { padding: 1, margin: 1, borderStyle: 'double' }));
+const { version } = require('./package.json');
+console.log(boxen('ProtonVPN OVPN Creds Generator', { padding: 1, margin: 1, borderStyle: 'double' }));
 console.log(chalk.bold('Coded by ' + chalk.magenta('leandev')));
+console.log(chalk.bold('Edits by ' + chalk.magenta('frostech')));
 console.log(chalk.bold('More info: ' + chalk.cyan('http://l34nd3v.com')));
 console.log(chalk.bold(chalk.red('This is an unofficial tool and is not affiliated with Proton Technologies AG\n')));
 
@@ -49,17 +52,14 @@ async function createAccount() {
   let user = createRandomString(12);
   let pasw = createRandomString(32);
 
-  await inquirer.prompt(options.confirmation).then(async a => {
-    if (!a.confirmation) {
-      await process.exit();
-    }
-  });
+
 
   const browser = await puppeteer.launch({
-    args: ['--disable-web-security', '--disable-features=IsolateOrigins,site-per-process', '--disable-dev-shm-usage']
+    args: ['--disable-web-security', '--disable-features=IsolateOrigins,site-per-process', '--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox'],
+    //headless: false,
   });
 
-  await console.log(chalk.bold(chalk.cyan('i ') + 'Generating your account...'));
+  await console.log(chalk.bold(chalk.cyan('i ') + 'Generation process is a go...'));
 
   const page_guerrillamail = await browser.newPage();
 
@@ -71,15 +71,15 @@ async function createAccount() {
   const page_proton = await browser.newPage();
   await page_proton.goto(proton_url);
 
-  await page_proton.waitForSelector('body > div.app-root > main > main > div > div:nth-child(5) > div:nth-child(1) > div.flex-item-fluid-auto.pt1.pb1.flex.flex-column > button').then(() => { }).catch(e => {
+  await page_proton.waitForSelector('body > div.app-root > main > main > div > div:nth-child(5) > div:nth-child(1) > div.flex-item-fluid-auto.pt1.pb1.flex-no-min-children.flex-column > button').then(() => { }).catch(e => {
     console.log(chalk.bold(chalk.red('X ') + 'Selector error'));
     browser.close();
   });
 
-  await page_proton.click('body > div.app-root > main > main > div > div:nth-child(5) > div:nth-child(1) > div.flex-item-fluid-auto.pt1.pb1.flex.flex-column > button');
+  await page_proton.click('body > div.app-root > main > main > div > div:nth-child(5) > div:nth-child(1) > div.flex-item-fluid-auto.pt1.pb1.flex-no-min-children.flex-column > button');
 
   const all_iframe_elements = await page_proton.$$('iframe');
-
+  await console.log(chalk.bold(chalk.cyan('i ') + 'Supplying credentials... (if this takes longer than 60 seconds, restart repl)'));
   const username_iframe_element = all_iframe_elements[0];
   const username_iframe = await username_iframe_element.contentFrame();
   await username_iframe.waitForSelector('#username');
@@ -89,52 +89,75 @@ async function createAccount() {
   const email_iframe = await email_iframe_element.contentFrame();
   const email_element = await email_iframe.$('#email');
 
-  await username_element.type(user, { delay: 30 });
-  await page_proton.type('#password.pm-field', pasw);
+  await username_element.type(user, { delay: 300 });
+  await page_proton.type('#password', pasw);
   await page_proton.type('#passwordConfirmation', pasw);
-  await email_element.type(mail, { delay: 30 });
+  await email_element.type(mail, { delay: 300 });
 
-  await page_proton.click('body > div > main > main > div > div.pt2.mb2 > div > div:nth-child(1) > form > div:nth-child(5) > div > button');
+  await page_proton.click('body > div.app-root > main > main > div > div.pt2.mb2 > div > div:nth-child(1) > form > div:nth-child(3) > div > button');
 
-  await page_proton.waitForSelector('body > div.app-root > main > main > div > div.pt2.mb2 > div > div.w100 > div:nth-child(2) > div > div > div:nth-child(2) > form > div:nth-child(2) > button').catch(e => {
+
+  await page_proton.waitForSelector('body > div.app-root > main > main > div > div.pt2.mb2 > div > div:nth-child(1) > form > div:nth-child(3) > div > button').catch(e => {
     console.log(chalk.bold(chalk.red('X ') + 'Selector error'));
     browser.close();
   });
 
-  await page_proton.click('body > div.app-root > main > main > div > div.pt2.mb2 > div > div.w100 > div:nth-child(2) > div > div > div:nth-child(2) > form > div:nth-child(2) > button');
+  await page_proton.waitForSelector('body > div.app-root > main > main > div > div.pt2.mb2 > div > div.w100 > div:nth-child(2) > div > div > div:nth-child(2) > form > div:nth-child(2) > button');
 
+  await page_proton.click('body > div.app-root > main > main > div > div.pt2.mb2 > div > div.w100 > div:nth-child(2) > div > div > div:nth-child(2) > form > div:nth-child(2) > button');
+  await console.log(chalk.bold(chalk.green('? ') + 'Credentials supplied.'));
+  await console.log(chalk.bold(chalk.cyan('i ') + 'Waiting for verification email... (this will take some time)'));
   await page_guerrillamail.bringToFront();
+
+  await page_guerrillamail.setDefaultTimeout(1000000);
 
   await page_guerrillamail.waitForFunction(getAuthCode)
 
   const auth_code = await page_guerrillamail.evaluate(getAuthCode)
-
+  await console.log(chalk.bold(chalk.green('? ') + 'Verification email recieved.'));
   await page_proton.bringToFront();
 
   await page_proton.type('#code', auth_code);
-  await page_proton.click('body > div.app-root > main > main > div > div.pt2.mb2 > div > div.w100 > div:nth-child(2) > form > div > div > div:nth-child(4) > button')
+  await page_proton.click('body > div.app-root > main > main > div > div.pt2.mb2 > div > div.w100 > div:nth-child(2) > form > div > div > div:nth-child(3) > button')
 
   let table = await new Table({
-    head: [chalk.green('Username'), chalk.green('Password'), chalk.green('Email')],
+    head: [chalk.green('ProtonVPN Username'), chalk.green('ProtonVPN Password'), chalk.green('ProtonVPN Email')],
     chars: {
-      'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
-      , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
-      , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
-      , 'right': '║', 'right-mid': '╢', 'middle': '│'
+      'top': '-', 'top-mid': '-', 'top-left': '+', 'top-right': '+'
+      , 'bottom': '-', 'bottom-mid': '-', 'bottom-left': '+', 'bottom-right': '+'
+      , 'left': 'ý', 'left-mid': 'ý', 'mid': '-', 'mid-mid': '+'
+      , 'right': 'ý', 'right-mid': 'ý', 'middle': 'ý'
     }
   });
 
   await table.push([user, pasw, mail]);
 
-  console.log(chalk.bold(chalk.green('✓ ') + 'Account generated'));
+  console.log(chalk.bold(chalk.green('? ') + 'Account generated (note the above are for protonvpn, not for blockiller.)'));
   console.log(table.toString());
-  console.log(chalk.bold(chalk.cyan('i ') + 'Verifying account...'));
+  console.log(chalk.bold(chalk.cyan('i ') + 'Grabbing OVPN details...'));
+  await page_proton.waitForSelector('body > div.app-root > div.content-container.flex.flex-column.flex-nowrap.no-scroll > div > div > div.main.ui-standard.flex.flex-column.flex-nowrap.flex-item-fluid > main > div > h1');
+  await page_proton.goto("https://account.protonvpn.com/account");
+  await page_proton.waitForSelector('body > div.app-root > div.content-container.flex.flex-column.flex-nowrap.no-scroll > div > div > div.main.ui-standard.flex.flex-column.flex-nowrap.flex-item-fluid > main > div > section:nth-child(9) > div > div:nth-child(4) > div.settings-layout-right.flex.flex-align-items-center > div.flex.flex-item-noshrink.on-mobile-mt0-5 > button:nth-child(2)');
+  await page_proton.click('body > div.app-root > div.content-container.flex.flex-column.flex-nowrap.no-scroll > div > div > div.main.ui-standard.flex.flex-column.flex-nowrap.flex-item-fluid > main > div > section:nth-child(9) > div > div:nth-child(4) > div.settings-layout-right.flex.flex-align-items-center > div.flex.flex-item-noshrink.on-mobile-mt0-5 > button:nth-child(2)');
+  const ovpnusr = await page_proton.evaluate(() => document.querySelector('body > div.app-root > div.content-container.flex.flex-column.flex-nowrap.no-scroll > div > div > div.main.ui-standard.flex.flex-column.flex-nowrap.flex-item-fluid > main > div > section:nth-child(9) > div > div:nth-child(3) > div.settings-layout-right.flex.flex-align-items-center > div.text-ellipsis.max-w100.mr1.on-mobile-mr0 > code').innerText);
+  const ovpnpsw = await page_proton.evaluate(() => document.querySelector('body > div.app-root > div.content-container.flex.flex-column.flex-nowrap.no-scroll > div > div > div.main.ui-standard.flex.flex-column.flex-nowrap.flex-item-fluid > main > div > section:nth-child(9) > div > div:nth-child(4) > div.settings-layout-right.flex.flex-align-items-center > div.text-ellipsis.max-w100.mr1.on-mobile-mr0 > code').innerText);
+  
+  let table2 = await new Table({
+    head: [chalk.green('blockiller Username'), chalk.green('blockiller Password')],
+    chars: {
+      'top': '-', 'top-mid': '-', 'top-left': '+', 'top-right': '+'
+      , 'bottom': '-', 'bottom-mid': '-', 'bottom-left': '+', 'bottom-right': '+'
+      , 'left': 'ý', 'left-mid': 'ý', 'mid': '-', 'mid-mid': '+'
+      , 'right': 'ý', 'right-mid': 'ý', 'middle': 'ý'
+    }
+  });
 
+  await table2.push([ovpnusr, ovpnpsw]);
+  console.log(chalk.bold(chalk.green('? ') + 'LETS FREAKING GOOOO, done'));
   setTimeout(async () => {
-    await browser.close()
-    await console.log(chalk.bold(chalk.green('✓ ') + 'Account verified'));
-    await console.log(chalk.bold(chalk.cyan('i ') + 'Exiting...'));
-    await process.exit();
+   await browser.close()
+   await console.log(chalk.bold(chalk.cyan('i ') + 'Exiting...'));
+   await process.exit();
   }, 20000);
 }
 
